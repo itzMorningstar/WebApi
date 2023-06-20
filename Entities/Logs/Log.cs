54a619535cbe06e63f22395f78d8c222c3dd228c
Shahreyar <shahreyar763@gmail.com>
2023-06-21 04:37:17 +0500

    log files added

---
+++ Entities/Logs/Log.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities.Logs;
+
+
+public class Log
+{
+    public int Id { get; set; }
+    public DateTime Timestamp { get; set; }
+    public string Level { get; set; }
+    public string Message { get; set; }
+    public string Exception { get; set; }
+}
